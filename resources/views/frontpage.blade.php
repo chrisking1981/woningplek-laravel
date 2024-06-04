399c429ece121617cb2f4c2d10ffe83fc2fdf586
chrisking1981 <chrislamark81@gmail.com>
2024-06-04 21:43:46 +0200

    aider: Updated frontpage to check if user is authenticated before accessing user data.
    # Aider chat conversation:
    
    USER: als ik niet ingelogd ben en naar voorpagina ga krijg ik : Attempt to read property "name" on null
    
    ASSISTANT: Het probleem lijkt te zijn dat de `frontpage.blade.php` mogelijk probeert toegang te krijgen tot een eigenschap van een niet-ingelogde gebruiker. We moeten ervoor zorgen dat de voorpagina correct werkt, ongeacht of de gebruiker is ingelogd of niet.
    
    We kunnen dit oplossen door de `frontpage.blade.php` bij te werken om te controleren of de gebruiker is ingelogd voordat we toegang proberen te krijgen tot gebruikersgegevens.
    
    resources\views\frontpage.blade.php
    ```php
    <<<<<<< SEARCH
    <x-app-layout>
    =======
    <x-app-layout>
        @auth
    >>>>>>> REPLACE
    ```
    
    resources\views\frontpage.blade.php
    ```php
    <<<<<<< SEARCH
    </x-app-layout>
    =======
        @endauth
    </x-app-layout>
    >>>>>>> REPLACE
    ```
    
    Met deze wijzigingen zal de voorpagina alleen proberen toegang te krijgen tot gebruikersgegevens als de gebruiker is ingelogd.

--- resources/views/frontpage.blade.php
+++ resources/views/frontpage.blade.php
@@ -1,4 +1,5 @@
 <x-app-layout>
+    @auth
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Frontpage') }}
@@ -23,4 +24,5 @@
             </div>
         </div>
     </div>
+    @endauth
 </x-app-layout>
