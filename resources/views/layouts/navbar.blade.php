bb2a2c5cdec4e8c134339c747c67e37a1fa5f912
chrisking1981 <chrislamark81@gmail.com>
2024-06-04 22:58:03 +0200

    aider: Reverted changes made to the navbar links to their original state.
    # Aider chat conversation:
    
    USER: ja alleen nu heb je de navbar ook gewijzigd dat wilde ik niet
    
    ASSISTANT: I will revert the changes made to the navbar to restore it to its original state.
    
    resources\views\layouts\navbar.blade.php
    ```php
    <<<<<<< SEARCH
                <a href="{{ route('login') }}" class="hover:underline">Inloggen</a>
                <a href="{{ route('register') }}" class="hover:underline">Account maken</a>
                <a href="#" class="bg-orange-500 text-white py-2 px-4 rounded hover:bg-orange-600">Plaats advertentie</a>
    =======
                <a href="#" class="hover:underline">Inloggen</a>
                <a href="#" class="hover:underline">Account maken</a>
                <a href="#" class="bg-orange-500 text-white py-2 px-4 rounded hover:bg-orange-600">Plaats advertentie</a>
    >>>>>>> REPLACE
    ```
    
    resources\views\layouts\navbar.blade.php
    ```php
    <<<<<<< SEARCH
                <a href="{{ route('frontpage') }}" class="flex items-center space-x-1 hover:underline">
                    <i class="fas fa-home"></i><span>Home</span>
                </a>
                <a href="{{ route('ads.show', 1) }}" class="flex items-center space-x-1 hover:underline">
                    <i class="fas fa-clock"></i><span>Recente advertenties</span>
                </a>
                <a href="{{ route('ads.store') }}" class="flex items-center space-x-1 hover:underline">
                    <i class="fas fa-plus"></i><span>Advertentie plaatsen</span>
                </a>
                <a href="#" class="flex items-center space-x-1 hover:underline">
                    <i class="fas fa-star"></i><span>Extra opvallen</span>
                </a>
                <a href="{{ route('profile') }}" class="flex items-center space-x-1 hover:underline">
                    <i class="fas fa-user"></i><span>Mijn account</span>
    =======
                <a href="#" class="flex items-center space-x-1 hover:underline">
                    <i class="fas fa-home"></i><span>Home</span>
                </a>
                <a href="#" class="flex items-center space-x-1 hover:underline">
                    <i class="fas fa-clock"></i><span>Recente advertenties</span>
                </a>
                <a href="#" class="flex items-center space-x-1 hover:underline">
                    <i class="fas fa-plus"></i><span>Advertentie plaatsen</span>
                </a>
                <a href="#" class="flex items-center space-x-1 hover:underline">
                    <i class="fas fa-star"></i><span>Extra opvallen</span>
                </a>
                <a href="#" class="flex items-center space-x-1 hover:underline">
                    <i class="fas fa-user"></i><span>Mijn account</span>
    >>>>>>> REPLACE
    ```

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -6,8 +6,8 @@
             <h1 class="text-xl font-bold">Woningplek</h1>
         </div>
         <div class="flex space-x-4">
-            <a href="{{ route('login') }}" class="hover:underline">Inloggen</a>
-            <a href="{{ route('register') }}" class="hover:underline">Account maken</a>
+            <a href="#" class="hover:underline">Inloggen</a>
+            <a href="#" class="hover:underline">Account maken</a>
             <a href="#" class="bg-orange-500 text-white py-2 px-4 rounded hover:bg-orange-600">Plaats advertentie</a>
         </div>
     </div>
@@ -17,19 +17,19 @@
 <nav class="bg-gray-800 text-white">
     <div class="container mx-auto px-4 py-2 flex justify-between items-center">
         <div class="flex space-x-4">
-            <a href="{{ route('frontpage') }}" class="flex items-center space-x-1 hover:underline">
+            <a href="#" class="flex items-center space-x-1 hover:underline">
                 <i class="fas fa-home"></i><span>Home</span>
             </a>
-            <a href="{{ route('ads.show', 1) }}" class="flex items-center space-x-1 hover:underline">
+            <a href="#" class="flex items-center space-x-1 hover:underline">
                 <i class="fas fa-clock"></i><span>Recente advertenties</span>
             </a>
-            <a href="{{ route('ads.store') }}" class="flex items-center space-x-1 hover:underline">
+            <a href="#" class="flex items-center space-x-1 hover:underline">
                 <i class="fas fa-plus"></i><span>Advertentie plaatsen</span>
             </a>
             <a href="#" class="flex items-center space-x-1 hover:underline">
                 <i class="fas fa-star"></i><span>Extra opvallen</span>
             </a>
-            <a href="{{ route('profile') }}" class="flex items-center space-x-1 hover:underline">
+            <a href="#" class="flex items-center space-x-1 hover:underline">
                 <i class="fas fa-user"></i><span>Mijn account</span>
             </a>
         </div>
