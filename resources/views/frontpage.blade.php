96933b37583b3ffa283670cd1c4bf6c7ce6dcd54
chrisking1981 <chrislamark81@gmail.com>
2024-06-04 22:01:34 +0200

    aider: Added navigation for guests to go back to the homepage, login, or register in `frontpage.blade.php` and `ads\show.blade.php`.
    # Aider chat conversation:
    
    USER: add navigation for guests so I can go back to the homepage or login or register
    
    ASSISTANT: To add navigation for guests so they can go back to the homepage or login or register, we need to update the `resources\views\frontpage.blade.php` and `resources\views\ads\show.blade.php` files to include navigation links.
    
    Here are the *SEARCH/REPLACE* blocks:
    
    resources\views\frontpage.blade.php
    ```php
    <<<<<<< SEARCH
        <div class="py-12">
    =======
        <nav class="bg-gray-800 p-4">
            <div class="container mx-auto flex justify-between">
                <a href="{{ url('/') }}" class="text-white">{{ __('Home') }}</a>
                <div>
                    <a href="{{ route('login') }}" class="text-white mr-4">{{ __('Login') }}</a>
                    <a href="{{ route('register') }}" class="text-white">{{ __('Register') }}</a>
                </div>
            </div>
        </nav>
        <div class="py-12">
    >>>>>>> REPLACE
    ```
    
    resources\views\ads\show.blade.php
    ```php
    <<<<<<< SEARCH
        <div class="py-12">
    =======
        <nav class="bg-gray-800 p-4">
            <div class="container mx-auto flex justify-between">
                <a href="{{ url('/') }}" class="text-white">{{ __('Home') }}</a>
                <div>
                    <a href="{{ route('login') }}" class="text-white mr-4">{{ __('Login') }}</a>
                    <a href="{{ route('register') }}" class="text-white">{{ __('Register') }}</a>
                </div>
            </div>
        </nav>
        <div class="py-12">
    >>>>>>> REPLACE
    ```

--- resources/views/frontpage.blade.php
+++ resources/views/frontpage.blade.php
@@ -1,4 +1,13 @@
 <x-guest-layout>
+    <nav class="bg-gray-800 p-4">
+        <div class="container mx-auto flex justify-between">
+            <a href="{{ url('/') }}" class="text-white">{{ __('Home') }}</a>
+            <div>
+                <a href="{{ route('login') }}" class="text-white mr-4">{{ __('Login') }}</a>
+                <a href="{{ route('register') }}" class="text-white">{{ __('Register') }}</a>
+            </div>
+        </div>
+    </nav>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
